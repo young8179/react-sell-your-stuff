36feedf283ffc2a3e3d7c408b5704d83cc473c0d
Heeyoung Song <hsong8179@gmail.com>
2021-05-25 12:03:39 -0400

    update

--- client/src/components/chatStore/Store.js
+++ client/src/components/chatStore/Store.js
@@ -6,36 +6,36 @@ export const ChatContext = createContext()
 
 
 const initState = {
-    Buy:[
-        {from: "Tap Bid", msg:"Hello, Welcome to Tap Bid"},
-       
+    Buy: [
+        { from: "Tap Bid", msg: "Hello, Welcome to Tap Bid" },
+
     ],
-    Sell:[
-        {from: "Tap Bid", msg:"Hello, Welcome to Tap Bid"},
-       
+    Sell: [
+        { from: "Tap Bid", msg: "Hello, Welcome to Tap Bid" },
+
     ],
-    Trade:[
-        {from: "Tap Bid", msg:"Hello, Welcome to Tap Bid"},
-        
+    Trade: [
+        { from: "Tap Bid", msg: "Hello, Welcome to Tap Bid" },
+
     ]
 }
 
-  
 
-function reducer(state, action){
-    const {from, msg, topic} = action.payload;
-    switch(action.type){
+
+function reducer(state, action) {
+    const { from, msg, topic } = action.payload;
+    switch (action.type) {
         //case set data
         case "RECEIVE_MESSAGE":
-            return{
+            return {
                 ...state,
-                [topic]:[
+                [topic]: [
                     ...state[topic],
                     { from, msg }
                 ]
             }
-            default:
-                return state
+        default:
+            return state
     }
 }
 
@@ -53,24 +53,24 @@ let socket = io()
 
 export default function Store(props) {
     const [chatContext, dispatch] = useReducer(reducer, initState)
- 
-    
+
+
     function sendChatAction(value) {
         socket.emit("message", value)
     }
 
-    
-    
-    
-    
-    useEffect(()=>{
-            socket.on("message", function (msg) {
-                dispatch({ type: "RECEIVE_MESSAGE", payload: msg })
-            })
-        
- },[])
 
- 
+
+
+
+    useEffect(() => {
+        socket.on("message", function (msg) {
+            dispatch({ type: "RECEIVE_MESSAGE", payload: msg })
+        })
+
+    }, [])
+
+
 
 
     return (
